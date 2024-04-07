94b4f9fb9596b04e91d45df8866856495ac55776
Seeds Odahviing <seedsoda@il-h6.msk.21-school.ru>
2024-04-07 21:42:18 +0300

    added db connection

---
+++ internals/repository/postgres.go
@@ -0,0 +1,35 @@
+package postgres
+
+import (
+	"fmt"
+	"log"
+	"tools/internals/cfg"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func NewPostgresDB(config *cfg.Configuration) (*sqlx.DB, error) {
+	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", config.DB.Host, config.DB.Port, config.DB.Name, config.DB.Password))
+	if err != nil {
+		log.Fatalf("Couldn't connect to DB:%v", err)
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		log.Printf("Received no response from DB:%v", err)
+	}
+	return db, nil
+}
+
+// create tables here
+// func main() {
+// 	config, err := cfg.LoadAndStore("../cfg")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	_, err = NewPostgresDB(config)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+
+// }
