0dea44fa552f3774ed3754833b6645011a86003f
Seeds Odahviing <seedsoda@il-h6.msk.21-school.ru>
2024-04-08 20:51:00 +0300

    improved general structure, added models

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
-	api "tools/api/middleware"
+	"log"
+	"tools/internals/cfg"
+	"tools/internals/handler"
+	"tools/internals/models"
+	"tools/internals/repository"
 	"tools/internals/server"
+	"tools/internals/service"
 )
 
 func main() {
+
+	config, err := cfg.LoadAndStore("../config")
+	if err != nil {
+		log.Fatalf("Couldn't parse the config file: %v", err)
+	}
+	db, err := repository.NewPostgresDB(config)
+	if err != nil {
+		log.Fatalf("Couldn't connect tp DB: %v", err)
+	}
+	rep := repository.NewRepository(db)
+	srvc := service.NewService(rep)
+	hdl := handler.NewHandler(srvc)
+	create := &models.Banner{Content: "\"title\":\"some_title\", \"text\":\"some_text\", \"url\":\"some_url\"", IsActive: true, TagIDs: []uint64{2, 11}, FeatureID: 2}
+	rep.AddBanner(create)
 	srv := new(server.Server)
-	srv.Run(api.InitRoutes())
+	if err = srv.Run(hdl.InitRoutes()); err != nil {
+		log.Fatalf("Couldn't start the server: %v", err)
+	}
 }
